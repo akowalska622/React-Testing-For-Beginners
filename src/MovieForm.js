a045f0d47b01f1bf49f2ecdd68c73cbfea34ad2f
Ania Kowalska <a.kowalska@qarbon.it>
2022-02-05 20:19:19 +0100

    spying and mocking functions in react

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function MovieForm() {
+export default function MovieForm({ submitForm }) {
+  const [text, setText] = useState('');
+
   return (
     <div>
-      <form data-testid='movie-form'>
-        <input type='text' />
+      <form data-testid='movie-form' onSubmit={() => submitForm(text)}>
+        <input type='text' onChange={(e) => setText(e)}/>
         <button>Submit</button>
       </form>
     </div>
--- src/NewMovie.test.js
+++ src/NewMovie.test.js
@@ -5,9 +5,11 @@ import NewMovie from './NewMovie';
 afterEach(cleanup);
 
 test('<NewMovie />', () => {
-  const { debug, getByTestId, queryByTestId, container } = render(<NewMovie />);
+  const { debug, getByTestId, queryByTestId, container, getByText } = render(<NewMovie />);
   expect(getByTestId('page-title').textContent).toBe('New Movie');
   expect(queryByTestId('movie-form')).toBeTruthy();
 
-  expect(container.firstChild).toMatchSnapshot()
+  expect(container.firstChild).toMatchSnapshot();
+
+  fireEvent.click(getByText('Submit'));
 });
