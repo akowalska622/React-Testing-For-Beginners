7fca6e440946a0b8d6e0c63e264361e38770d79b
Ania <a.kowalska622@gmail.com>
2022-02-05 14:26:25 +0100

    Integration Testing In React

---
+++ src/MovieForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function MovieForm() {
+  return (
+    <div>
+      <form data-testid='movie-form'>
+        <input type='text' />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
