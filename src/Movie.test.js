6cc69858720fe396bd371b63a229f031e8aa7f95
Ania <a.kowalska622@gmail.com>
2022-02-07 19:32:25 +0100

    Negative assertion and testing react router dom

--- src/Movie.test.js
+++ src/Movie.test.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
+import { MemoryRouter } from 'react-router-dom';
 import Movie from './Movie';
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup();
+  console.error.mockClear();
+});
 
 console.error = jest.fn();
 
 test('<Movie />', () => {
-  render(<Movie />)
+  render(<Movie />);
 
   // if we don't mock expected error, we'll got error in test
-  expect(console.error).toBeCalled();
-})
+  expect(console.error).toHaveBeenCalled();
+});
+
+const movie = {
+  id: 'hi',
+  title: 'Level Up Big Day Out',
+  poster_path: 'fsdsafdfdasg.jpg',
+};
+
+test('<Movie /> with movie', () => {
+  render(
+    <MemoryRouter>
+      <Movie movie={movie} />
+    </MemoryRouter>
+  );
+
+  // if we don't mock expected error, we'll got error in test
+  expect(console.error).not.toHaveBeenCalled();
+});
